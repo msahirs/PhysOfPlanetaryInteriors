a5c529091d2654c62191ee3b46128dee28edc8b3
ssujahudeen <sujahudeen.sahir@gmail.com>
2023-05-11 13:55:13 +0200

    removed debug statements

--- Assignment_1/src/celestial_body.py
+++ Assignment_1/src/celestial_body.py
@@ -12,7 +12,7 @@ def rk4(dydx, x0, y0, x_f, steps):
     # step height h
     hist = [y0]
     h = abs(((x_f - x0)/steps))
-
+    
     # Iterate for number of iterations
     y = y0
     for i in range(1, steps):
@@ -45,9 +45,10 @@ class Celestial:
     def _rho_func(self,r):
 
         for i in self.layers.values():
-            
             if r >= i.r_bounds[0] and r < i.r_bounds[1]:
+                print(i.rho_type)
                 return i.get_rho(r)
+            
         return 0
 
     def add_layer(self,layer_name,layer):
@@ -78,33 +79,58 @@ class Celestial:
         p_range = rk4(p_func,r_probe,0,self.top_bound,steps)
         
         return p_range[::-1]
-        
 
+# TESTS DEFINITION ##
+
+def test_func_1():
     
-earth = Celestial(name="Earth")
+    earth = Celestial(name="Earth")
 
-earth.add_layer("f1", InternalLayer_1D(0, 6378000/2,
-                         "constant",
-                         y_int = 10000, slope = -50.3,
-                         const_rho = 5515,
-                         func = lambda x: 4**x))
+    earth.add_layer("f1", InternalLayer_1D(0, 6378000/4,
+                            "linear",
+                            y_int = 0, slope = 5515/(6378000/4 - 0),
+                            const_rho = 5515,
+                            func = lambda x: 4**x))
 
-earth.add_layer("f2", InternalLayer_1D(6378000/2, 6378000,
-                         "linear",
-                         y_int = 5515, slope = -0.001,
-                         const_rho = 3000,
-                         func = lambda x: 4**x))
+    earth.add_layer("f2", InternalLayer_1D(6378000/4, 6378000/2,
+                            "constant",
+                            y_int = 0, slope = 5515/(6378000 - 6378000*0.9999),
+                            const_rho = 5515,
+                            func = lambda x: 4**x))
+    
+    earth.add_layer("f3", InternalLayer_1D(6378000/2, 6378000,
+                            "linear",
+                            y_int = 5515, slope = 5515/(6378000 - 6378000/2),
+                            const_rho = 5515,
+                            func = lambda x: 4**x))
+    
+    
 
-# mmoi = earth.get_mmoi()
-# r_range = np.linspace(0.1,6378000*1.2,10000).tolist()
+    # earth.add_layer("f2", InternalLayer_1D(6378000/2, 6378000,
+    #                         "linear",
+    #                         y_int = 5515, slope = 0.001,
+    #                         const_rho = 3000,
+    #                         func = lambda x: 4**x))
 
-# g_range = [earth.get_g(r) for r in r_range]
-r_range_2 = np.linspace(0.1,6378000,10000).tolist()
-p_ran = earth.get_p_range(0.1,10000)
+    # mmoi = earth.get_mmoi()
+    r_range = np.linspace(0.1,6378000,10000).tolist()
+
+    # g_range = [earth.get_g(r) for r in r_range]
+    r_range_2 = np.linspace(0.1,6378000,1000).tolist()
+    p_ran = earth.get_p_range(0.1,1000)
 
 
 
-# rho_range = [earth._rho_func(r) for r in r_range]
-# mass_range = [earth.get_mass(r) for r in r_range]
-plt.plot(r_range_2,p_ran)
-plt.show()
+    # rho_range = [earth._rho_func(r) for r in r_range]
+    
+    # mass_range = [earth.get_mass(r) for r in r_range]
+    # plt.plot(r_range,rho_range)
+
+    # print("mmoi factor:", mmoi/(mass_range[-1] * earth.top_bound**2))
+    plt.plot(r_range_2,p_ran)
+    # plt.plot(r_range_2,p_ran)
+    plt.show()
+
+## TEST RUNNING ##
+
+test_func_1()
