413afeea61a6fe6ab86d65ea7b65a1b503e191a2
ssujahudeen <sujahudeen.sahir@gmail.com>
2023-06-16 00:29:56 +0200

    pushed final_1.inp. use this as input!

--- Assignment_2/helper_files/plot_analytical.py
+++ Assignment_2/helper_files/plot_analytical.py
@@ -17,27 +17,34 @@ def exp_decrement(t,t_r,initial_disp):
 # SCRIPT START
 
 #Input params of crust and loading
-rho_cr = 3000
-nu = 3.333E-24
-g_surf = 1.9144
-lamb = 2 * 1000E3
-w = 4000
+
+rho_cr = 3000   # Crustal Density
+nu = 3.333E-24  # Mantle Visocsity
+g_surf = 1.9144     # Surface gravitational strength
+lamb = 2 * 1000E3   # Loading wavelength (2*crater diam)
+w = 1813    # Inital depression
 
 # Get values
-t_r = time_const(rho_cr,nu,g_surf,lamb)
-t_range = np.linspace(0,10*t_r,100)
+t_r = time_const(rho_cr, nu, g_surf,lamb)
+t_range = np.linspace(0,10*t_r,50)
 w_range = exp_decrement(t_range,t_r,w)
+
+print("Time constant: %.3E" % t_r)
+print(f"Time Interval for analytical: {t_range[1]:.3E}", )
 
 # PLOTTING
 
-# ABAQUS calculation
-plt.plot(t_range, w_range,
-         'x--', color = 'blue', label = "ABAQUS Model")
+# # ABAQUS calculation
+# plt.plot(t_range, w_range,
+#          'x--', color = 'blue', alpha = 0.7,
+#          label = "ABAQUS Model")
 
 # Analytical calculation
 plt.plot(t_range, w_range,
-         color = 'orange', label = "Turcotte and Schubert")
+         color = 'orange', alpha = 0.7, 
+         label = f"Turcotte and Schubert, \n $t_r = {t_r:.3E} $")
 
+plt.plot(1.050E+13,1462, "x")
 
 plt.title("Numerical (ABAQUS) vs Analytical Solutions")
 plt.xlabel("Elapsed time [t]")
